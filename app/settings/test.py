f41718c0d71ace237aaf867a9cc17493209cec1f
MihaTeam1 <sarsern2004@gmail.com>
2022-08-01 14:29:31 +0300

    change workflow

--- app/settings/test.py
+++ app/settings/test.py
@@ -5,8 +5,7 @@ from typing import List
 
 
 class Settings(BaseSettings):
-    allowed_hosts: List['str'] = ["127.0.0.1", "localhost"]
-    origins: List['str'] = ["http://127.0.0.1", "http://localhost"]
+    pass
 
     class Config:
         env_file = '.env/test'
