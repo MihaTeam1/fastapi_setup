970cc4a529b0e7de5be500f09f45bb14cc8165e5
MihaTeam1 <sarsern2004@gmail.com>
2022-08-01 14:22:40 +0300

    change test settings

--- app/settings/test.py
+++ app/settings/test.py
@@ -1,10 +1,12 @@
 from pydantic import (
     BaseSettings,
 )
+from typing import List
 
 
 class Settings(BaseSettings):
-    pass
+    allowed_hosts: List['str'] = ["127.0.0.1", "localhost"]
+    origins: List['str'] = ["http://127.0.0.1", "http://localhost"]
 
     class Config:
         env_file = '.env/test'
